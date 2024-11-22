6fc2c641321285164a71fd0cddaaa3fafc0ea50e
Phillip Suh <phillipsuh@ou.edu>
2024-11-22 00:08:29 -0600

    Add project files.

---
+++ PersonalWebsite/Pages/Skills.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhillipSuhPortfolio.Pages
+{
+    public class SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
